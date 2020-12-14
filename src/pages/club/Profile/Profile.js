6355034c8bc16352529ae516861bbf1c500c67d2
Shashwat Gupta <shashwatsatna@gmail.com>
2020-12-14 22:56:33 +0530

    add background hoc

--- src/pages/club/Profile/Profile.js
+++ src/pages/club/Profile/Profile.js
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  // Button,
+  // Card,
+  // CardContent,
+  // Dialog,
+  // DialogActions,
+  // DialogContent,
+  // Grid,
+  // Hidden,
+  makeStyles,
+  // TextField,
+  // Typography,
+} from "@material-ui/core";
+import Background from "../../../hoc/Background/Background";
+
+const useStyles = makeStyles((theme) => ({}));
 
 const ClubProfile = () => {
-	return <div>Club Profile</div>;
+  const classes = useStyles();
+  return (
+    <Background>
+      <div>Club Profile</div>
+    </Background>
+  );
 };
 
 export default ClubProfile;
--- src/routes/ClubRoutes.jsx
+++ src/routes/ClubRoutes.jsx
@@ -3,15 +3,19 @@ import { Switch, Route } from "react-router-dom";
 import Dashboard from "../pages/club/Dashboard/Dashboard";
 import ClubSignin from "../pages/club/SignIn";
 import SignUp from "../pages/club/SignUp";
+import AddQuestions from "../pages/club/AddQuestion/AddQuestion";
+import Profile from "../pages/club/Profile/Profile";
 
 const ClubRoutes = () => {
-	return (
-		<Switch>
-			<Route exact path="/club/signin" component={ClubSignin} />
-			<Route exact path="/club/signup" component={SignUp} />
-			<Route exact path="/club/dashboard" component={Dashboard} />
-		</Switch>
-	);
+  return (
+    <Switch>
+      <Route exact path='/club/signin' component={ClubSignin} />
+      <Route exact path='/club/signup' component={SignUp} />
+      <Route exact path='/club/dashboard' component={Dashboard} />
+      <Route exact path='/club/profile' component={Profile} />
+      {/* <Route exact path='/club/addQuestions' component={AddQuestions} /> */}
+    </Switch>
+  );
 };
 
 export default ClubRoutes;
